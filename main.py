a62520c27b763f6fd58b399ed11adefba2ec89f3
matthew everard <64141240+ematthewephec@users.noreply.github.com>
2021-11-14 19:04:01 +0100

    MVP
    add: Premieres fonctionnalités

---
+++ main.py
@@ -0,0 +1,22 @@
+from whatsmyip.ip import get_ip
+from whatsmyip.providers import GoogleDnsProvider
+
+
+myip=get_ip(GoogleDnsProvider)
+
+print(myip)
+
+
+"""
+    while True:
+        print("entrer votre commande")
+        commande=input()
+            if commande = "stop"
+                break
+            elif commande="ip"
+                print(myip)
+            else
+                print("une erreur à été detecté")
+"""
+
+
