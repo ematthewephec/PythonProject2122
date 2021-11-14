fe26157eb1fea054a90fda6628c8c1205b309cff
Leon <l.servais@students.ephec.be>
2021-11-14 20:56:10 +0100

    cli test

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from whatsmyip.ip import get_ip
+"""from whatsmyip.ip import get_ip
 from whatsmyip.providers import GoogleDnsProvider
 
 
 myip=get_ip(GoogleDnsProvider)
 
 print(myip)
+"""
+import fire
+from getpass import getpass
+class Auth (object):
+    def login(self, username = None):
+        if username  == None:
+            username = input ("Username: ")
+        if username == None:
+            print ("A username is required")
+            return
+        pw = getpass("Password: ")
+        return username, pw
+
+def login (username = None):
+    if username == None:
+        username = input ("Username: ")
+    if username == None:
+        print ("A username is required")
+        return
+    pw = getpass ("Password: ")
+    return username, pw
 
 
 """
