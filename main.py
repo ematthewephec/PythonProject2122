1ddc95306c06299b492633e8b0f95223fb0c1139
matthew everard <64141240+ematthewephec@users.noreply.github.com>
2021-11-22 10:27:22 +0100

    MVP project
    Add: modifications et déboggage

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from whatsmyip.ip import get_ip
 from whatsmyip.providers import GoogleDnsProvider
 
-
-myip=get_ip(GoogleDnsProvider)
+myip = get_ip(GoogleDnsProvider)
 
 print(myip)
 
-
-    while True:
-        print("entrer votre commande")
-        commande=input()
-            if commande = "stop":
-                break
-            elif commande= "ip" :
-                print(myip)
-            else :
-                print("une erreur à été detecté")
-
-
+while True:
+    print("Entrer votre commande")
+    commande = input()
+    if commande == "stop":
+        print("Arret du programme")
+        break
+    elif commande == "ip":
+        print("votre adresse ip est: " + myip)
+    else:
+        print("une erreur à été detecté")
 
