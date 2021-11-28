388bf2ee624f923f231673723938d978ac172999
PiretArnaud <HE201852@students.ephec.be>
2021-11-28 20:36:19 +0100

    ajout des premières fonctions et classe "HelpFunction"

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from whatsmyip.ip import get_ip
 from whatsmyip.providers import GoogleDnsProvider
 
-myip = get_ip(GoogleDnsProvider)
 
-print(myip)
-
-while True:
-    print("Entrer votre commande")
-    commande = input()
-    if commande == "stop":
-        print("Arret du programme")
-        break
-    elif commande == "ip":
+def getip(cmd):
+    if len(cmd.split()) == 1:
+        myip = get_ip(GoogleDnsProvider)
         print("votre adresse ip est: " + myip)
+    elif len(cmd.split()) > 1 and cmd.split()[1] == "help":
+        print(helpGetIp.help)
     else:
         print("une erreur à été detecté")
 
+
+class HelpFunction:
+    def __init__(self, description):
+        self.help = description
+
+
+helpGetIp = HelpFunction("cette fonction ne prend pas d'argument et renvoie votre adresse ip public")
+functionList = "voici le liste des fonctions :"+" /getIp"
+helpGeneral = HelpFunction(functionList+". ajoutez \" help\" à la suite de celles-ci pour plus d'informations")
+
+while True:
+    print("Entrer votre commande:")
+    commande = input()
+    if commande == "/stop":
+        print("Arret du programme")
+        break
+    elif commande != "" and commande.split()[0] == "/getIp":
+        getip(commande)
+    elif commande != "" and commande.split()[0] == "/help":
+        print(helpGeneral.help)
+    else:
+        print("une erreur à été detecté")
