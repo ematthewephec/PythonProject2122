097c2577e4a71b4508b5197d6a75e3b2807be782
Leon <l.servais@students.ephec.be>
2021-12-19 00:14:22 +0100

    UI Update with get command and show output

--- UI/UI.py
+++ UI/UI.py
@@ -1,6 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 
 
 Builder.load_file('UI.kv')
@@ -11,6 +17,7 @@ class UI(Widget):
 
 
 class UIApp(App):
+    test3 = ObjectProperty(None)
     def build(self):
         return UI()
 
@@ -18,9 +25,20 @@ class UIApp(App):
         text = self.root.ids.input.text
         print(text)
 
+
     def outputtext(self):
-        text = self.root.ids.input.text
-        print(text)
+        text = self.root.ids.output.text
+
+        self.ids['result_label'].text = str("fuck you")
+
+
+
+    def _on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
+        if self.test3.focus and keycode == 40:  # 40 - Enter key pressed
+            self.abc()
+
+    def abc(self):
+        print('Test')
 
 if __name__ == "__main__":
     UIApp().run()
