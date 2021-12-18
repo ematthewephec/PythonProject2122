3626eda892ecf45f544e796448473ed561aa5119
PiretArnaud <HE201852@students.ephec.be>
2021-12-19 00:30:43 +0100

    attention ! break the code UI need to receive data

--- appellechat.py
+++ appellechat.py
@@ -1,11 +1,14 @@
 from main import *
 
-def inputcommand(command):
+def inputcommand():
+    command = input()
     if command != "" and command.split()[0] == "/getIp":
         getip(command)
     elif command != "" and command.split()[0] == "/help":
         print(helpGeneral.help)
     elif command != "" and command.split()[0] == "/showOnlineUserTag":
         showOnlineUserTag(command,userlist)
+    elif command != "" and command.split()[0] == "/version":
+        print(versionAPP)
     else:
-        return "commande non existante , faites /help pour voir la liste des commandes "
+        print("commande non existante , faites /help pour voir la liste des commandes" )
